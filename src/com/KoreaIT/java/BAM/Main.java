47dc9d4365881ca5636f08e8b59b840e3fc141ab
lifenell0411 <lifenell0411@gmail.com>
2022-11-21 11:45:22 +0900

    무한루프에서 명령어 받기

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
-		System.out.printf("명령어)");
+
 		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
+		while (true) {
 
-		System.out.printf("입력된 문장 : %s \n ", str);
-
-		int cmd_INT = sc.nextInt();
-
-		System.out.printf("입력된 정수 : %d \n ", str);
+			System.out.printf("명령어)");
+			String command = sc.nextLine();
+			
+			if (command.equals("exit")) {
+				break;
+			}
+		}
 
 		System.out.println("==프로그램 끝==");
 		sc.close();
