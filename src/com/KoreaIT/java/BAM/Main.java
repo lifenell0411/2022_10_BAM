c3f7fced8b99f222da5973ed377e34fcfa38ebde
lifenell0411 <lifenell0411@gmail.com>
2022-11-23 10:45:46 +0900

    게시물리스트 구현중 (arraylist 활용)

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -1,5 +1,7 @@
 package com.KoreaIT.java.BAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class Main {
@@ -7,26 +9,34 @@ class Main {
 		System.out.println("==프로그램 시작==");
 		Scanner sc = new Scanner(System.in);
 		int lastArticleId = 0;
-		
-		
+		List<Article> articles = new ArrayList<>();
+
 		while (true) {
 			System.out.printf("명령어)");
-			String command = sc.nextLine();
-			
-			
+			String command = sc.nextLine().trim();
+
 			if (command.equals("article write")) {
+				int id = lastArticleId + 1;
+				lastArticleId = id;
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-				
-				System.out.println("글이 생성되었습니다");
-				
-				
+
+				Article article = new Article(id, title, body);
+				articles.add(article);
+
+				System.out.printf("%d번글이 생성되었습니다\n", id);
+
 			} else if (command.equals("article list")) {
-				System.out.println("게시글이 없습니다.");
-			
-			}else {
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다.");
+
+				} else {
+					System.out.println("게시글이 있습니다.");
+
+				}
+			} else {
 				System.out.println("존재하지 않는 명령어입니다");
 			}
 			if (command.length() == 0) {
@@ -39,5 +49,18 @@ class Main {
 		}
 		System.out.println("==프로그램 종료==");
 		sc.close();
+	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+
 	}
 }
